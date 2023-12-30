9b0647031ed910d49e48f6d892d6e62715321c84
Mick van Gelderen <mickvangelderen@gmail.com>
2023-12-30 23:57:55 +0100

    Animate!

--- ncurses.c
+++ ncurses.c
@@ -2,10 +2,14 @@
 #include <locale.h>
 #include <wchar.h>
 #include <assert.h>
+// #include <stdio.h>
+#include <time.h>
 
 const char *PATH = "  ";
 const char *GOAL = "🚩";
 const char *PLAYER = "🚶";
+const char *PLAYER2 = "🏃";
+
 const char *WALL = "🌲";
 const char *START = "📌";
 
@@ -18,13 +22,41 @@ void printMaze(const char *maze[MAZE_ROWS][MAZE_COLS])
     {
         for (int j = 0; j < MAZE_COLS; j++)
         {
-            // mvprintw((i + 4), (j + 3), "%s ", maze[i][j]);
             mvaddstr(4 + i, 3 + j * 2, maze[i][j]);
         }
     }
 }
 // https://stackoverflow.com/questions/61690230/chinese-character-too-large-for-enclosing-character-literal-type
 // https://man7.org/linux/man-pages/man3/wcwidth.3.html
+
+typedef struct Point2I
+{
+    int x;
+    int y;
+} Point2I;
+
+typedef struct Player {
+    Point2I pos;
+    int anim;
+} Player;
+
+void render(const char *maze[MAZE_ROWS][MAZE_COLS], Player player)
+{
+    clear();
+
+    // Print time.
+    time_t rawtime;
+    time(&rawtime);
+    struct tm *timeinfo;
+    timeinfo = localtime(&rawtime);
+    mvprintw(1, 1, "%s", asctime(timeinfo));
+
+    printMaze(maze);
+    const char * graphic = player.anim < 30 ? PLAYER : PLAYER2;
+    mvaddstr(4 + player.pos.y, 3 + player.pos.x * 2, graphic);
+
+    return 0;
+}
 
 int main()
 {
@@ -39,11 +71,17 @@ int main()
     assert(wcwidth(U'📌') == 2);
 
     // Player position.
-    int y = 1, x = 1;
+    Player player = {
+        .pos = {
+            .x = 1,
+            .y = 1,
+        },
+        .anim = 0,
+    };
 
     // Initialize the screen buffer for ncurses.
     initscr();
-    keypad(stdscr, 1); 
+    keypad(stdscr, 1);
 
     noecho();    // do not show the user input
     curs_set(0); // hide blinking cursor
@@ -60,26 +98,26 @@ int main()
 
     while (true)
     {
-        // Draw.
-        clear();
-        printMaze(maze);
-        mvprintw(4 + y, 3 + x * 2, "🚶");
+        render(maze, player);
 
         // Process input.
+        timeout(16);
         int c = getch();
 
         if (c == 27)
             break;
         if (c == KEY_UP)
-            y--;
+            player.pos.y--;
         if (c == KEY_DOWN)
-            y++;
+            player.pos.y++;
         if (c == KEY_LEFT)
-            x--;
+            player.pos.x--;
         if (c == KEY_RIGHT)
-            x++;
+            player.pos.x++;
+
+        player.anim = (player.anim + 1) % 60;
     }
-    
+
     endwin();
 
     return 0;
