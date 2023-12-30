c025412db3b95a21b6dec5a4d07e7aba1b18a174
AtJiawei <jiaweitian92@gmail.com>
2023-12-30 17:19:28 +0100

    yay

---
+++ maze05.c
@@ -0,0 +1,139 @@
+// this version is to add ncurses.h library 
+#include <stdio.h>
+#include <ncurses.h>
+
+// define the global variables
+const char *PATH = "🌿";
+const char *GOAL = "🚩";
+const char *PLAYER = "🚶";
+const char *WALL = "🧱";
+
+const int MAZE_ROWS = 7;
+const int MAZE_COLS = 7;
+
+typedef struct
+{
+    int x;
+    int y;
+} Position;
+
+Position findPlayer(const char *maze[MAZE_ROWS][MAZE_COLS])
+{
+    Position playerPosition = {0, 0};
+    for (int i = 0; i < MAZE_ROWS; i++)
+    {
+        for (int j = 0; j < MAZE_COLS; j++)
+        {
+            if (maze[i][j] == PLAYER)
+            {
+                playerPosition.x = i;
+                playerPosition.y = j;
+                return playerPosition;
+            }
+        }
+    }
+    return playerPosition;
+}
+
+int updateMaze(char Move, const char *maze[MAZE_ROWS][MAZE_COLS])
+{
+    Position playerPosition = findPlayer(maze);
+    int newX = playerPosition.x;
+    int newY = playerPosition.y;
+
+    if (Move == 'w')
+    {
+        newX--;
+    }
+    if (Move == 's')
+    {
+        newX++;
+    }
+    if (Move == 'a')
+    {
+        newY--;
+    }
+    if (Move == 'd')
+    {
+        newY++;
+    }
+
+    if (newX >= 0 && newX < MAZE_ROWS && newY >= 0 && newY < MAZE_COLS && maze[newX][newY] != WALL)
+    {
+        if (maze[newX][newY] == GOAL)
+        {
+            maze[playerPosition.x][playerPosition.y] = PATH;
+            maze[newX][newY] = PLAYER;
+            return 1; // Player reached goal
+        }
+        else
+        {
+            maze[playerPosition.x][playerPosition.y] = PATH;
+            maze[newX][newY] = PLAYER;
+            return 0; // Normal move
+        }
+    }
+    return 0;
+}
+
+void printMaze(const char *maze[MAZE_ROWS][MAZE_COLS])
+{
+    printf("\n");
+    for (int i = 0; i < MAZE_ROWS; i++)
+    {
+        for (int j = 0; j < MAZE_COLS; j++)
+        {
+            printf("%s ", maze[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void play(const char *maze[MAZE_ROWS][MAZE_COLS])
+{
+    char Move;
+    while (1)
+    {
+        printf("\n____ Enter w/s/a/d to control the avatar, enter q to exit the game (please only use lower case): ");
+        scanf(" %c", &Move);
+
+        if (Move == 'q')
+        {
+            printf("\n|| Game Ended.\n\n|| You have chosen to quit the game by pressing q.\n\n");
+            break; // end the loop if 'q' is pressed
+        }
+        else if (Move == 'w' || Move == 's' || Move == 'a' || Move == 'd')
+        {
+            if (updateMaze(Move, maze))
+            {
+                printMaze(maze);
+                printf("\n|| Congratulations! \n\n|| You have WON!\n\n");
+                break;
+            }
+            printMaze(maze);
+        }
+        else
+        {
+            printf("Invalid key entered.\n");
+        }
+    }
+}
+
+int main()
+{
+    // Initialize the maze
+    const char *maze[MAZE_ROWS][MAZE_COLS] =
+        {
+            {WALL, WALL, WALL, WALL, WALL, WALL, WALL},
+            {WALL, PLAYER, PATH, WALL, PATH, WALL, WALL},
+            {WALL, WALL, PATH, WALL, PATH, PATH, WALL},
+            {WALL, PATH, PATH, PATH, PATH, WALL, WALL},
+            {WALL, PATH, WALL, WALL, PATH, PATH, WALL},
+            {WALL, PATH, PATH, WALL, WALL, PATH, WALL},
+            {WALL, WALL, WALL, WALL, WALL, GOAL, WALL}};
+
+    // Print the maze
+    printMaze(maze);
+    play(maze);
+    return 0;
+}
--- ncurses.c
+++ ncurses.c
@@ -1,11 +1,44 @@
 #include <ncurses.h>
+#include <locale.h>
 
 int main()
 {
-    initscr();
-    printw("hello, world");
-    getchar();
-    refresh();
+    setlocale(LC_ALL, "");
+
+    int c;
+    int y = 11, x = 11; // @coordinates
+    char i;             // our symbol
+
+    initscr(); // initial curses
+    keypad(stdscr, 1);
+
+    noecho();    // do not show the user input
+    curs_set(0); // hide blinking cursor
+
+    mvprintw(0, 6, "Chose any numeric or character key as your avatar by clicking them once. Then use arrows to move the avatar around\n");
+    mvprintw(1, 11, "Press Esc to exit the game\n");
+    i = getch(); // get symbol
+
+    do
+    {
+        clear();
+        mvprintw(0, 6, "Chose any numeric or character key as your avatar by clicking them once. Then use arrows to move the avatar\n");
+        mvprintw(1, 11, "Press Esc to exit the game\n");
+        if (c == KEY_UP)
+            y--;
+        if (c == KEY_DOWN)
+            y++;
+        if (c == KEY_LEFT)
+            x--;
+        if (c == KEY_RIGHT)
+            x++;
+
+        mvprintw(y, x, "🚶");
+        // mvaddch(y, x, i); // be careful that this starts with y. put symbol at location (y,x)
+    } while ((c = getch()) != 27); // 27 'Esc'
+
+    // refresh();// needed when there is not a while loop. refresh the location
+    // getch(); // needed when there is not a while loop so that the window will not refresh and immediately close -> you can then see the change
     endwin();
 
     return 0;
